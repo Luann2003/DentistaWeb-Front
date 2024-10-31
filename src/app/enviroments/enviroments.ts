4a5e98a2d979501a30e7b08d102dc0f8f8e62bfa
Luann2003 <lvictorluciano@outlook.com>
2024-10-31 10:13:22 -0300

    env

--- src/app/enviroments/enviroments.ts
+++ src/app/enviroments/enviroments.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:8080',  // URL da API de desenvolvimento
-    apiKey: 'AIzaSyD7kRNFzOk3fCJCv0vojY9ngGEoXbZ3l4E'
+    apiKey: ''
   };
   
